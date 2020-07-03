9552d434164eefb836c67f0a8eae25d738d68d9a
Vipul Chandan <plenteousvipul2020@gmail.com>
2020-07-03 22:29:30 +0530

    Clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello.</h1>
       <h2>My self Vipul Chandan. I am a programer from Kurse</h2>
-      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
     </Layout>
   )
 }
